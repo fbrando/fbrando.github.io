420c5ccdd0084f31bb4aa6fb3914a74b2e55981d
Brandon F <bfredley@gmail.com>
2018-06-09 17:01:01 -0600

    L07 Grid

--- assignments/lesson-7/js/windchill.js
+++ assignments/lesson-7/js/windchill.js
@@ -1,11 +1,10 @@
-//var h = div.getElementById("high").value;
-//var l = parseFloat(document.getElementById("low").value);
-//var s = parseFloat(document.getElementById("wind-speed").value);
+var h = parseInt(document.getElementById("high").innerHTML);
+var l = parseInt(document.getElementById("low").innerHTML);
+var s = parseInt(document.getElementById("wind-speed").innerHTML);
 
-var t = (78 + 56) / 2;
-var s = 10;
-var f = (35.74 + (0.6215 * t)) - (35.75 * Math.pow(s, .16)) + (0.4275 * t * Math.pow(s, .16));
+var t = (h + l) / 2;
+var f = (35.74 + (0.6215 * t)) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16));
 
-document.getElementById("windchill").innerHTML = Math.round(f, 10);
+document.getElementById("windchill").innerHTML = Math.round(f,10);
 
 /* where f is the wind chill factor in Fahrenheit, t is the air average temperature in Fahrenheit, and s is the wind speed in miles per hour.*/
